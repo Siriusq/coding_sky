0bb3d4a6c967b38e3a874778d2b7126aa5be0a94
Siriusq <eargon.skyle@gmail.com>
2021-11-28 04:54:21 +0000

    Quiz Rewrite

--- coding/urls.py
+++ coding/urls.py
@@ -8,12 +8,13 @@ app_name = 'coding'
 urlpatterns = [
     path('', index,name='index'),
     path("login", login, name="login"),  
+    path("logout", logout, name="logout"),  
     path("dashboard", dashboard, name="dashboard"),  
-    #path("quiz", quiz, name="quiz"),  
+    path("quiz", quiz, name="quiz"),  
     path("register", register, name="register"),  
-    #path("create-quiz", create_quiz, name="create-quiz"),  
-    #path("create-questions", create_question, name="create-question"),  
-    #path("answer-quiz/<slug>", answer_quiz, name="answer-quiz"), 
+    path("create_quiz", create_quiz, name="create_quiz"),  
+    path("create_questions", create_question, name="create_question"),  
+    path("answer_quiz/<slug>", answer_quiz, name="answer_quiz"), 
     path('game/', gamePage,name='gamePage'),
     path('solution/', solutionPage,name='solutionPage'),
 ]
