dfd1dd0c728a94c9aa9e24a4cb4543e4d9a50e43
Siriusq <eargon.skyle@gmail.com>
2021-11-30 03:35:13 +0000

    IDE Solution Added!

---
+++ coding/static/js/ace_config.js
@@ -0,0 +1,68 @@
+var editor0 = ace.edit('code');
+    editor0.setTheme("ace/theme/dracula");
+    editor0.session.setMode("ace/mode/python");
+    editor0.setFontSize(18);
+    editor0.resize();
+
+var editor1 = ace.edit('code1');
+    editor1.setTheme("ace/theme/dracula");
+    editor1.session.setMode("ace/mode/python");
+    editor1.setReadOnly(true);
+    editor1.setFontSize(18);
+    editor1.resize();
+
+var editor2 = ace.edit('code2');
+    editor2.setTheme("ace/theme/dracula");
+    editor2.session.setMode("ace/mode/python");
+    editor2.setReadOnly(true);
+    editor2.setFontSize(18);
+    editor2.resize();
+
+var editor3 = ace.edit('code3');
+    editor3.setTheme("ace/theme/dracula");
+    editor3.session.setMode("ace/mode/python");
+    editor3.setReadOnly(true);
+    editor3.setFontSize(18);
+    editor3.resize();
+
+var editor4 = ace.edit('code4');
+    editor4.setTheme("ace/theme/dracula");
+    editor4.session.setMode("ace/mode/python");
+    editor4.setReadOnly(true);
+    editor4.setFontSize(18);
+    editor4.resize();
+
+var editor5 = ace.edit('code5');
+    editor5.setTheme("ace/theme/dracula");
+    editor5.session.setMode("ace/mode/python");
+    editor5.setReadOnly(true);
+    editor5.setFontSize(18);
+    editor5.resize();
+
+var editor6 = ace.edit('code6');
+    editor6.setTheme("ace/theme/dracula");
+    editor6.session.setMode("ace/mode/python");
+    editor6.setReadOnly(true);
+    editor6.setFontSize(18);
+    editor6.resize();
+
+var editor7 = ace.edit('code7');
+    editor7.setTheme("ace/theme/dracula");
+    editor7.session.setMode("ace/mode/python");
+    editor7.setReadOnly(true);
+    editor7.setFontSize(18);
+    editor7.resize();
+
+var editor8 = ace.edit('code8');
+    editor8.setTheme("ace/theme/dracula");
+    editor8.session.setMode("ace/mode/python");
+    editor8.setReadOnly(true);
+    editor8.setFontSize(18);
+    editor8.resize();
+
+var editor9 = ace.edit('code9');
+    editor9.setTheme("ace/theme/dracula");
+    editor9.session.setMode("ace/mode/python");
+    editor9.setReadOnly(true);
+    editor9.setFontSize(18);
+    editor9.resize();
