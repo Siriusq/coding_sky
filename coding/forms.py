ab3b0c9bda06147e3d6ca45f627b1e955b081dac
Siriusq <eargon.skyle@gmail.com>
2021-12-01 22:13:05 +0000

    Quiz Style Upgraded

--- coding/forms.py
+++ coding/forms.py
@@ -8,7 +8,9 @@ class QuestionForm(forms.Form):
     def __init__(self, question, *args, **kwargs):
         super(QuestionForm, self).__init__(*args, **kwargs)
         choice_list = [x for x in question.get_answers_list()]
-        self.fields["answers"] = forms.ChoiceField(choices=choice_list, widget=RadioSelect)
+        self.fields["answers"] = forms.ChoiceField(
+			choices=choice_list, widget=RadioSelect(attrs={'class': 'mr-2 align-middle', 'type': 'radio'})
+		)
 
 class NewUser(UserCreationForm):
 	username = forms.CharField(
